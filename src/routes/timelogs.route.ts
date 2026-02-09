e7950d84ea25f84337e76f3c9939f82d10f6b6a2
Isidro R. Bernadas <sid@Isidros-MacBook-Air.local>
2026-02-09 16:02:41 +0800

    removed auth in time in/out

--- src/routes/timelogs.route.ts
+++ src/routes/timelogs.route.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { rbacMiddleware } from "../middleware/rbac.middleware";
 import { createTimeLogController } from "../controllers/timelogs.controller";
 
 const router = Router();
 
 router.use(authMiddleware);
 
-router.get("/log", rbacMiddleware(["ADMIN", "EMPLOYEE"]), createTimeLogController);
+router.get("/log", createTimeLogController);
 
 export default router;
