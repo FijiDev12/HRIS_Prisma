5850685141b2fa2fde2636f4ce3ca2c3504dc461
FijiGVI <carljerson.oquila@globalvisionsinc.com.ph>
2026-03-19 09:04:35 +0800

    test

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
+  // Datasource URLs go here
+  datasource: {
+    url: process.env.DATABASE_URL,           // runtime (pooled)
+    shadowDatabaseUrl: process.env.DIRECT_URL, // migrations (direct)
+  },
+
   migrations: {
     path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
   },
 });
--- src/util/prisma.util.ts
+++ src/util/prisma.util.ts
@@ -1,27 +1,9 @@
-// import "dotenv/config";
-// import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-// import { PrismaClient } from '../../generated/prisma/client';
-
-// const adapter = new PrismaMariaDb({
-//   host: process.env.DATABASE_HOST,
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-//   connectionLimit: 5
-// });
-// const prisma = new PrismaClient({ adapter });
-
-// export { prisma }
-
 import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
-const pool = new pg.Pool({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
-
-const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
